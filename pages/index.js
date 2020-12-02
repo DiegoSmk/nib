4bcd8739fc510602e00c2d6f48036a8e6be48dfe
Diego Pena <diego.nib@outlook.com>
2020-12-01 21:08:18 -0300

    fist push

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
+import React, { useState } from 'https://esm.sh/react'
+// import Logo from '../components/logo.tsx'
+function Home() {
+    return <div>Home1</div>
+}
+
+export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,8 @@
+// import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
+import React, { useState } from 'https://esm.sh/react'
+// import Logo from '../components/logo.tsx'
+function Home() {
+    return <div>Sobre</div>
+}
+
+export default Home
