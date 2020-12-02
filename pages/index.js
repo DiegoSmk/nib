41c67a442be89e58a7ffdee8ccc44525c0c5daf0
Diego Pena <diego.nib@outlook.com>
2020-12-01 21:57:25 -0300

    reset / alt project for next js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-// import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
-import React, { useState } from 'https://esm.sh/react'
-// import Logo from '../components/logo.tsx'
 function Home() {
-    return <div>Home1</div>
+  return <div>Home</div>
 }
 
-export default Home
+export default Home 
--- pages/sobre.js
+++
@@ -1,8 +0,0 @@
-// import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
-import React, { useState } from 'https://esm.sh/react'
-// import Logo from '../components/logo.tsx'
-function Home() {
-    return <div>Sobre</div>
-}
-
-export default Home
